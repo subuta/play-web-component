747541efe0a674f4f2b89c5899e8a8447363adf8
shunta saito <s.shunta@goodpatch.com>
2016-10-16 12:04:05 +0200

    [modify]add initial files.

---
+++ jspm.config.js
@@ -0,0 +1,34 @@
+SystemJS.config({
+  paths: {
+    "npm:": "jspm_packages/npm/",
+    "play-web-component/": "src/"
+  },
+  browserConfig: {
+    "baseURL": "/"
+  },
+  devConfig: {
+    "map": {
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.16"
+    }
+  },
+  transpiler: "plugin-babel",
+  packages: {
+    "play-web-component": {
+      "main": "app.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-babel"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {},
+  packages: {}
+});
